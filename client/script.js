ff53e73f3c24e743f03ae175c0561e677f136e62
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-28 18:36:33 -0800

    loader element updated, link rel stylesheet spelling error fixed

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ function loader(element) {
     loadInterval = setInterval(() => {
         element.textContent += '.';
 
-        if (element.textContent === '....') {
+        if (element.textContent === '......') {
             element.textContent = '';
         }
     }, 300)
