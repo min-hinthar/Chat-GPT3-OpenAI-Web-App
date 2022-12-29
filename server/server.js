c0b77387b31cb4f845419987d34556b542f64935
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-28 16:02:53 -0800

    OPENAI_API_KEY stored inside .env, server.js updated with apiKey configurations, app.get, app.post routes created

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+    res.status(200).send({ 
+        message: 'Connected to ChatGPT Codex',
+    })
+});
+
+// beta.openai.com/playground/node.js documentation
+app.post('/', async(req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+});
