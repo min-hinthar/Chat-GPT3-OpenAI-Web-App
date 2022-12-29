a3328a70031a49fed538cd0bfd77255667a8d8e2
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-28 19:14:27 -0800

    uploaded user favicon as user image updated in script.js

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import user from './assets/user.ico';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
